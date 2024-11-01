e5ffdb7c9ed8bbc3cf146817fe862a6e3f064945
Akash S <akashspalloor@gmail.com>
2024-11-01 13:19:37 +0530

    add event for deltebtn to remove list

--- script.js
+++ script.js
@@ -20,5 +20,22 @@ addBtn.addEventListener("click", () => {
         inputBox.value = ""  // Clear the input field
         inputBox.focus()     // Set focus back to the input field for easy typing
 
+        let deleteBtn = document.createElement('i')
+        deleteBtn.classList.add("fa-solid", "fa-trash")
+        li.appendChild(deleteBtn)
+
+        let editBtn = document.createElement('i')
+        editBtn.classList.add("fa-solid", "fa-pen-to-square")
+        li.appendChild(editBtn)
+
+        // Add an event listener to the delete button
+        deleteBtn.addEventListener('click', () => {
+            li.style.animation = "slideOut 0.5s ease-in-out";
+            li.addEventListener("animationend", () => {
+                li.remove()
+                thoughtArray = thoughtArray.filter(item => item !== inputValue);
+                console.log(thoughtArray);
+            })
+        });
     }
 })

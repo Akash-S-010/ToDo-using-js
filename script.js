4f9562c81eac19cfcf55e538c6b5012b87477b4a
Akash S <akashspalloor@gmail.com>
2024-11-01 13:57:29 +0530

    add event to edit list

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ let thoughtArray = []
 
 addBtn.addEventListener("click", () => {
     let inputValue = inputBox.value
+
     if (inputValue == "") {
         alert("List Cannot be empty")
+
     } else {
         let li = document.createElement("li")
         li.innerHTML = inputValue
         ul.appendChild(li)
 
+        // created an array for store inputValues
         thoughtArray.push(inputValue)
         console.log(thoughtArray)
 
         li.style.animation = "slideIn 0.3s ease-in-out"
 
+
         inputBox.value = ""  // Clear the input field
         inputBox.focus()     // Set focus back to the input field for easy typing
+
 
         let deleteBtn = document.createElement('i')
         deleteBtn.classList.add("fa-solid", "fa-trash")
@@ -28,14 +33,31 @@ addBtn.addEventListener("click", () => {
         editBtn.classList.add("fa-solid", "fa-pen-to-square")
         li.appendChild(editBtn)
 
+
         // Add an event listener to the delete button
         deleteBtn.addEventListener('click', () => {
             li.style.animation = "slideOut 0.5s ease-in-out";
             li.addEventListener("animationend", () => {
                 li.remove()
-                thoughtArray = thoughtArray.filter(item => item !== inputValue);
+                // when removing a list it also removed from the array
+                thoughtArray = thoughtArray.filter(item => item !== inputValue)
                 console.log(thoughtArray);
             })
+        })
+
+        // Add an event listener to the edit button
+        editBtn.addEventListener("click", () => {
+            let editValue = prompt("Please change The Thought")
+            if (editValue !== null && editValue.trim() !== "") { 
+                li.firstChild.nodeValue = editValue
+                // Updated the value also from the thought array
+                const currentIndex = thoughtArray.indexOf(inputValue)
+                if (currentIndex !== -1) {
+                    thoughtArray[currentIndex] = editValue;
+                }
+                console.log(thoughtArray);
+            }
         });
+        
     }
 })
